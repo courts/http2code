4d20f662558dc538d52849e264d3525e9c1aec7c
Patrick Hof <courts@offensivethinking.org>
2012-04-04 14:45:40 +0200

    Added bacon tests for request.rb

---
+++ test/request_spec.rb
@@ -0,0 +1,36 @@
+require 'bacon'
+require_relative '../lib/request.rb'
+
+describe Request do
+  before do
+    options = {
+      :delimiter => "\r\n",
+      :template  => nil,
+      :header_bl => false,
+      :pretty    => true
+    }
+    @req_get = Request.new(File.open('http_get.txt').read(), options)
+    @req_post = Request.new(File.open('http_post.txt').read(), options)
+  end
+
+  it 'should parse headers' do
+    @req_get.header.should == {
+      "Host"            => "http://www.example.org",
+      "User-Agent"      => "Mozilla/5.0 (X11; Linux i686; rv:11.0) Gecko/20100101 Firefox/11.0",
+      "Accept"          => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Accept-Language" => "en-us,en;q=0.5",
+      "Accept-Encoding" => "gzip, deflate",
+      "Connection"      => "keep-alive"
+    }
+
+    @req_get.body.should == {}
+  end
+
+  it 'should parse a POST body' do
+    @req_post.body.should == {
+      "param1" => ["1", "4"],
+      "param2" => ["2"],
+      "param3" => ["3"]
+    }
+  end
+end
